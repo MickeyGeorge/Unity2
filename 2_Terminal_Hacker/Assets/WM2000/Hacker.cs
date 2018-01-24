4f26bfd84314c465eaab0286602aba52b133ff91
MickeyGeorge <jankmj@hotmail.com>
2018-01-24 16:30:51 -0600

    Initial Commit

---
+++ 2_Terminal_Hacker/Assets/WM2000/Hacker.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hacker : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        ShowMainMenu();
+    }
+
+    void ShowMainMenu() {
+
+        string greeting = "Hello Michael";
+
+        Terminal.ClearScreen();
+        Terminal.WriteLine(greeting);
+        Terminal.WriteLine("What would you like to hack into?");
+        Terminal.WriteLine("");
+        Terminal.WriteLine("Press 1 for the local library");
+        Terminal.WriteLine("Press 2 for the police station");
+        Terminal.WriteLine("Press 3 for NASA");
+        Terminal.WriteLine("");
+        Terminal.WriteLine("Enter your selection: ");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+}
